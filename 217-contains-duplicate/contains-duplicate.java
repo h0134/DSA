b432fd1bcd3c3801e2914770b8695ea820bf8d10
h0134 <114075408+h0134@users.noreply.github.com>
2024-04-24 13:20:33 +0530

    Time: 21 ms (10.07%) | Memory: 55.2 MB (86.64%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,15 +1,12 @@
 class Solution {
    
-    public static boolean containsDuplicate(int[] nums) {
+public static boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
-
-    for(int i=0;i<nums.length-1;i++){
-         if(nums[i]==nums[i+1]){
-               return true;
-                               }
-                               }
-    return false;
-}
-
-
-    }
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+                }
+                }
+                return false;
+                }
+                }

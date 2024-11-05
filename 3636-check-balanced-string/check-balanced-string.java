e4e3980a03366c174416d5acca3e2f701d324095
h0134 <114075408+h0134@users.noreply.github.com>
2024-11-05 06:40:34 +0530

    Time: 1 ms (99.60%) | Memory: 41.8 MB (98.56%) - LeetSync

--- 3636-check-balanced-string/check-balanced-string.java
+++ 3636-check-balanced-string/check-balanced-string.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isBalanced(String num) {
         int [] prefixSum = new int [num.length()]; 
-          int[] result = new int[num.length()];
+        int[] result = new int[num.length()];
 
         for (int i = 0; i < num.length(); i++) {
             result[i] = num.charAt(i) - '0';
@@ -9,8 +9,7 @@ class Solution {
         prefixSum[0]=result[0]; prefixSum[1]=result[1];
         for(int i=2; i <result.length;i++){
             prefixSum[i]=result[i]+prefixSum[i-2];
-
-        }
+            }
 
         if(prefixSum[num.length()-1]==prefixSum[num.length()-2]){
             return true;

5241be683cbf0ec501f29ca0f0c0be974b20ba12
h0134 <114075408+h0134@users.noreply.github.com>
2024-11-20 01:45:37 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (77.77%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,7 +1,7 @@
 class Solution {
     public int rob(int[] arr) {
   int n = arr.length;
-        int[] dp = new int [n+1];
+        int[] dp = new int [n];
         if(n==1){
             return arr[0];
         }

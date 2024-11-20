cd15f12d76b91594fe684325a99f6fcd1f7ff4ee
h0134 <114075408+h0134@users.noreply.github.com>
2024-11-20 05:44:12 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (68.87%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,46 +1,50 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int left =0;
-        int right=rows-1;
-        int floor=-1;
-        while(left<=right){
-           int  mid=(left+right)/2;
-            if(target==matrix[mid][0]){
-                floor=mid;
-                break;
-            }
-            else if(target>matrix[mid][0]){
-                left=mid+1;
-            }
-            else if(target<matrix[mid][0]){
-                right=mid-1;
-            }}
-            if(floor==-1){
+        int nrows = matrix.length;
+        int ncols = matrix[0].length;
+        int left=0;
+        int right=nrows-1;
+        int floor =-1;
+    while(left<=right){
+        int mid =(left+right)/2;
 
-                floor= left-1;
-                
-                }
-                System.out.println(floor);
-   if(floor<0){
-    floor =0;
-   }
-           left =0;
-           right = cols-1;
-           while(left<=right){
-            int mid = (left+right)/2;
-            if(target ==matrix[floor][mid]){
-                return true;
+        if(target==matrix[mid][0]){
+            floor= mid;
+            break;
+            
+      }
+        else if(target>matrix[mid][0]){
+            left=mid+1;
+        }
+           else if(target<matrix[mid][0]){
+            right=mid-1;
+        }
+        
+    }
+    if(floor==-1){
+    floor=left-1;
 
-            }
-            else if (target>matrix[floor][mid]){
-                left=mid+1;
-            }
-              else if (target<matrix[floor][mid]){
-                right=mid-1;
-            }
-           }
-           return false;      
+ }
+ if(floor==-1){
+    floor=0;
+ }
+
+     left=0;
+     right=ncols-1;
+
+while(left<=right){
+     int mid =(left+right)/2;
+
+        if(target==matrix[floor][mid]){
+            return true;
+            
+      }
+        else if(target>matrix[floor][mid]){
+            left=mid+1;
+        }
+           else if(target<matrix[floor][mid]){
+            right=mid-1;
+        }}
+return false;
     }
 }

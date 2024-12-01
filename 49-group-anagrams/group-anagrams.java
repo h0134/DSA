d49bdbb199a324efb244bd1ca57c03935649df42
h0134 <114075408+h0134@users.noreply.github.com>
2024-12-01 16:10:47 +0530

    Time: 7 ms (64.00%) | Memory: 48.2 MB (20.23%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,31 +1,30 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String,List<String>> mymap = new HashMap<>();
-        List<List<String>> result = new ArrayList<>();
-        
+        Map<String ,List<String>> mymap = new HashMap<>();
+        List<List<String>> y = new ArrayList<>();
         for(String word:strs){
-        char [] charArray = word.toCharArray();
-        Arrays.sort(charArray);
-        String str = new String(charArray);
-
-
-            if(mymap.containsKey(str)){
-                mymap.get(str).add(word);
-
-                }
-            else{
-                List<String> resultt = new ArrayList<>();
-                resultt.add(word);
-                mymap.put(str, resultt );
+            char[] arr = word.toCharArray();
+            Arrays.sort(arr);
+            String st = new String(arr);
+            if(!mymap.containsKey(st)){
+                List<String> re= new ArrayList<>();
+                re.add(word);
+                 mymap.put(st,re);
             }
+            else{
+           List<String> r=  mymap.get(st);
+           r.add(word);
+            mymap.put(st,r);
+
+            }
+
         }
+ 
+ for(Map.Entry<String,List<String>> entry: mymap.entrySet()){
+    y.add(entry.getValue());
 
-        for(Map.Entry<String,List<String>> entry: mymap.entrySet()){
-            List<String> values = entry.getValue();
-            result.add(values);
-        }
-        return result;
-
-     }
-
+ }
+ return y;
+     
+    }
 }

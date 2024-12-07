35eb8d45724f45391ac1448551eabf02b3d2fc12
h0134 <114075408+h0134@users.noreply.github.com>
2024-12-07 23:51:28 +0530

    Time: 12 ms (92.42%) | Memory: 44.1 MB (75.24%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -3,17 +3,15 @@ class Solution {
         int [] dp = new int [amount+1];
         Arrays.fill(dp,Integer.MAX_VALUE);
         dp[0]=0;
-        for(int coin:coins){
-            for(int i=1;i<=amount;i++){
+        for(int coin: coins){
+            for(int i =1; i<=amount;i++){
                 if(i>=coin){
                     if(dp[i-coin]!=Integer.MAX_VALUE){
-                       dp[i]=Math.min(dp[i-coin]+1,dp[i]);
-                                     }
+                   dp[i]=Math.min(dp[i],dp[i-coin]+1);
+ }
                 }
             }
         }
-
-return dp[amount]!=Integer.MAX_VALUE?dp[amount]:-1;
-        
+        return dp[amount]==Integer.MAX_VALUE?-1:dp[amount];
     }
 }

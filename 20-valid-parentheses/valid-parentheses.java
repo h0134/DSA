d78eeced837797556939e8258f30045c3a6c8bfe
h0134 <114075408+h0134@users.noreply.github.com>
2024-12-07 22:55:31 +0530

    Time: 2 ms (97.48%) | Memory: 41.7 MB (39.65%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,36 +1,41 @@
 class Solution {
     public boolean isValid(String s) {
-                Stack<Character> stack = new Stack<>();
-                for(int i=0; i <s.length();i++){
-                    if(s.charAt(i)=='('||s.charAt(i)=='['||s.charAt(i)=='{'){
-                        stack.add(s.charAt(i));
-                    }
-                    else{
-
-                    if(stack.isEmpty()){
-                       return false;
-
-                        }
-                    char top= stack.peek();
-
-                    if(top=='(' && s.charAt(i)!=')'){
-                        return false;
-
-                        }
-                    if(top=='[' && s.charAt(i)!=']'){
-                        return false;
-                          }
-                  
-                    if(top=='{' && s.charAt(i)!='}'){
-                        return false;
-                          }
-                          stack.pop();
-
-                    }
-                 
+        Stack<Character> stack = new Stack<>();
+        for(char ch :s.toCharArray()){
+            if(ch== '(' || ch=='[' || ch =='{'){
+             stack .add(ch) ;  
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
                 }
-                return stack.isEmpty()?true:false;
+                char topmost = stack.peek();
+
                 
-   
+            if(topmost=='(' &&  ch ==')'){
+                stack.pop();
+            }
+            else if(topmost=='[' &&  ch ==']'){
+stack.pop();
+                }
+           else if(topmost=='{' &&  ch =='}'){
+stack.pop();
+                }
+                else{
+                    return false;
+                }
+            
+            
+        }
+    
     }
+       if(stack.isEmpty()){
+            return true;
+        }
+    
+            return false;
+    
+
+    }
+
 }
